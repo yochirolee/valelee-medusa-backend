9a076ae570df304a79ce56a759d8abcae49eaeb6
Yochiro lee <yleecruz@gmail.com>
2025-08-07 16:25:48 -0400

    creando admin

--- .medusa/server/server.js
+++ .medusa/server/server.js
@@ -1,4 +1,4 @@
-const { createServer } = require('@medusajs/framework/server')
+const { createServer } = require('@medusajs/framework')
 
 const port = process.env.PORT || 9000
 const host = process.env.HOST || '0.0.0.0'
