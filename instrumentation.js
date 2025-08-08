ffdce5863c5aa723e20c66f9701bc9d3bba4c36a
Yochiro lee <yleecruz@gmail.com>
2025-08-08 12:19:29 -0400

    creando admin

---
+++ instrumentation.js
@@ -0,0 +1,7 @@
+// Simple instrumentation file to satisfy Medusa CLI requirements
+function register() {
+  // Empty function - instrumentation is disabled
+  console.log('Instrumentation disabled')
+}
+
+module.exports = { register }
